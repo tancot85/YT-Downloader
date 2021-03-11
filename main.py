a8068fd5c45eb6da849622dc8444288018edf9f6
Tanmay Tete <tanmay.tete@somaiya.edu>
2021-03-12 01:14:06 +0530

    added get_stream and download_stream functions

--- main.py
+++ main.py
@@ -4,7 +4,24 @@ import pprint
 URL = input('enter the video url: ')
 
 video = pafy.new(URL)
+all_streams = video.allstreams
+# print(video.title)
+# pprint.pprint(video.allstreams)
+ 
+def get_stream(video):
+    for i in all_streams:
+        print(all_streams.index(i), ". ", i)
+    option = int(input('enter the index of the quality you want: '))
 
-print(video.title)
+    return all_streams[option]
 
-pprint.pprint(video.allstreams)
+def download_stream(video):
+    get_stream_video = get_stream(video)
+    downloading_video_index = all_streams.index(get_stream_video) 
+    print('video name: ',video.title,' by: ',video.author)
+    print('size: ',video.allstreams[downloading_video_index].get_filesize()/1000000 , 'mb')
+    video.allstreams[downloading_video_index].download()
+
+# download_stream(video)
+
+download_stream(video)
